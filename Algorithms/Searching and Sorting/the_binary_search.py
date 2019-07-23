888cc073fe2d372ab372b6d04200660bcd2a1bf3
David Tsai <32302812+dtsai7@users.noreply.github.com>
2019-07-23 21:00:41 +0800

    Update the_binary_search.py

--- Algorithms/Searching and Sorting/the_binary_search.py
+++ Algorithms/Searching and Sorting/the_binary_search.py
@@ -1,13 +1,13 @@
-def binary_search(list, item):
+def binary_search(alist, item):
 	first = 0
-	last = len(list)- 1
+	last = len(alist)- 1
 	found = False
 
 	while first <= last and not found:
 		midpoint = (first+last)//2
-		if list[midpoint] == item:
+		if alist[midpoint] == item:
 			found = True
-		elif item < list[midpoint]:
+		elif item < alist[midpoint]:
 			last = midpoint- 1
 		else: 
 			first = midpoint + 1
@@ -19,4 +19,16 @@ alist = [1, 3, 4, 12, 14, 19, 22, 28, 39, 41]
 print(binary_search(alist, 19))
 print(binary_search(alist, 27))
 
-
+#-------------Recursive Version------------------------------
+def binarySearch(alist, item):
+	if len(alist) == 0:
+		return False
+	else:
+		midpoint = len(alist)//2
+		if alist[midpoint]== item:
+	          return True
+	        else:
+	          if item< alist[midpoint]:
+	            return binarySearch(alist[:midpoint], item)
+	          else:
+	            return binarySearch(alist[midpoint+1:], item)
